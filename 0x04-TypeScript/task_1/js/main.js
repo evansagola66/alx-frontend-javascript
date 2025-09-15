d784a31993f54f1b4bf3a19df3768a872c1961a1
bitevans <evansagola6@gmail.com>
2025-09-15 19:16:13 +0000

    all

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,10 @@
+// main.ts
+// Example usage
+var teacher3 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    fullTimeEmployee: false,
+    location: 'London',
+    contract: false, // extra property not defined in interface but allowed
+};
+console.log(teacher3);
